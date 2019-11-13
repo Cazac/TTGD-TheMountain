39f3d21b859ccf6f8c3ca29a72f9cfb62c7f2ed4
Zac <zachary.blouin@gmail.com>
2019-11-12 22:06:12 -0800

    Setup 0.4
    - More imported assets

---
+++ KPU - The Mountain/Assets/Imported Assets/_Models/Voxel Medieval Blacksmith/Assets/Scripts/BellowsAnimScript.cs
@@ -0,0 +1,118 @@
+﻿// **********
+// Released by JustUsGamers.net
+// Under the CC0 - Creative Commons Zero license
+// No attribution is required, feel free to share this freely
+// JustUsGamers.net holds no responsibility for the use of this code
+// and provides no warranty.
+// **********
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellowsAnimScript : MonoBehaviour {
+
+    private enum State { Empty, Half, Full }
+    private State BellowsState = State.Empty;
+
+    public float BellowsCycleLength = 3f;
+    public float BellowsFillSpeed = .3f;
+    public bool isBellowsAnimating;
+
+    private Renderer[] bellowsRenderers;
+    private bool isInflating = true;
+    private float currentCycleTime = 0f;
+
+	void Start () {
+
+        // Get all the Renderers of our 3 Bellows states (Full, Half, Empty)
+        // Then run BellowsChange to get everything started.
+
+        bellowsRenderers = this.GetComponentsInChildren<Renderer>(true);
+        BellowsChange();
+	}
+
+    private void Update()
+    {
+        // If our Bellows is supposed to be animating then add the time since the last frame
+        // and see if it's been enough time to cycle again.
+        if (isBellowsAnimating)
+        {
+            currentCycleTime += Time.deltaTime;
+
+            if (currentCycleTime >= BellowsCycleLength)
+            {
+                currentCycleTime = 0;
+                BellowsChange();
+            }
+
+        }
+
+    }
+
+    /// <summary>
+    /// Handles the states for Inflating and Deflating the Bellows
+    /// Essentially we are Invoking this function with a delay to give our
+    /// Bellows the look and animation of it Inflating and Deflating.
+    /// </summary>
+    void BellowsChange()
+    {
+        if (isInflating)
+        {
+            switch (BellowsState)
+            {
+                case State.Empty:
+                    BellowsState = State.Half;
+                    Invoke("BellowsChange", BellowsFillSpeed / 2);
+
+                    break;
+                case State.Half:
+                    BellowsState = State.Full;
+
+                    bellowsRenderers[0].enabled = false;
+                    bellowsRenderers[1].enabled = true;
+                    bellowsRenderers[2].enabled = false;
+
+                    Invoke("BellowsChange", BellowsFillSpeed / 2);
+
+                    break;
+                case State.Full:
+                    bellowsRenderers[0].enabled = false;
+                    bellowsRenderers[1].enabled = false;
+                    bellowsRenderers[2].enabled = true;
+                    isInflating = !isInflating;
+
+                    break;
+            }
+        }
+        else
+        {
+            switch (BellowsState)
+            {
+                case State.Empty:
+                    bellowsRenderers[0].enabled = true;
+                    bellowsRenderers[1].enabled = false;
+                    bellowsRenderers[2].enabled = false;
+                    isInflating = !isInflating;
+
+                    break;
+                case State.Half:
+                    BellowsState = State.Empty;
+
+                    bellowsRenderers[0].enabled = false;
+                    bellowsRenderers[1].enabled = true;
+                    bellowsRenderers[2].enabled = false;
+
+                    Invoke("BellowsChange", BellowsFillSpeed / 2);
+
+                    break;
+                case State.Full:
+                    BellowsState = State.Half;
+                    Invoke("BellowsChange", BellowsFillSpeed / 2);
+
+                    break;
+            }
+        }
+    }
+
+}
---
+++ KPU - The Mountain/Assets/Imported Assets/_Models/Voxel Medieval Blacksmith/Assets/Scripts/FirelightWobble.cs
@@ -0,0 +1,70 @@
+﻿// **********
+// Released by JustUsGamers.net
+// Under the CC0 - Creative Commons Zero license
+// No attribution is required, feel free to share this freely
+// JustUsGamers.net holds no responsibility for the use of this code
+// and provides no warranty.
+// **********
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirelightWobble : MonoBehaviour {
+
+    // Adjust these in the editor to affect how the light wobbles.
+    public float WobbleIntensity = .05f;
+    public float WobbleSpeed = 20f;
+    public float LightIntensity = 2f;
+
+    private Vector3 homePosition;
+    private Vector3 nextPosition;
+    private bool finishedWobble = false;
+    private Light theLight;
+    private float newIntensity;
+
+    private void Start()
+    {
+        // Gets the Light and its Home Position
+        // Then calls NewWobble() to get things started.
+
+        theLight = this.GetComponent<Light>();
+        homePosition = this.transform.position;
+        NewWobble();
+    }
+
+    void Update () {
+        // We want to see if our current Wobble has finished.
+        // If yes, then get a new one started
+        // if not, Lerp to destination.
+
+        theLight.intensity = Mathf.Lerp(theLight.intensity, newIntensity, Time.deltaTime * 4f);
+
+        if (finishedWobble)
+        {
+            NewWobble();
+            finishedWobble = false;
+        }
+
+        this.transform.position = Vector3.Lerp(this.transform.position, nextPosition, WobbleSpeed * Time.deltaTime);
+
+        if (Vector3.Distance( this.transform.position, nextPosition) <= WobbleIntensity * .1f)
+        {
+            finishedWobble = true;
+        }
+	}
+
+    /// <summary>
+    /// Sets nextPosition to a random location based around the lights
+    /// home position. This is affected by WobbleIntensity.
+    /// </summary>
+    private void NewWobble()
+    {
+        nextPosition = homePosition + new Vector3(
+            Random.Range(-WobbleIntensity, WobbleIntensity),
+            Random.Range(-WobbleIntensity, WobbleIntensity),
+            Random.Range(-WobbleIntensity, WobbleIntensity));
+
+        newIntensity = LightIntensity + ( (nextPosition.y - homePosition.y) / WobbleIntensity / 4);
+    }
+}
