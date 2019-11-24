22271b9b34cbf14235e291452cb83dd14181fcf3
Zac <zachary.blouin@gmail.com>
2019-11-23 18:53:59 -0800

    WorldGen 0.10
    - Asset Cleanup
    - Room Generation / Themes / Loot

---
+++ KPU - The Mountain/Assets/_Scripts/World Generation/TM_Theme.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TM_Theme : MonoBehaviour
+{
+
+
+    public List<TM_LootNode> RoomLootNodes_List;
+
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
