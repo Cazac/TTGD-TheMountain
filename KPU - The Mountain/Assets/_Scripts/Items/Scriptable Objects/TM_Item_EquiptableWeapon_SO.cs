86e5b78c582b16f240fe3c9aed46246244acd47a
Zac <zachary.blouin@gmail.com>
2020-01-15 19:50:45 -0800

    Interaction 0.03
    - Added more item types

---
+++ KPU - The Mountain/Assets/_Scripts/Items/Scriptable Objects/TM_Item_EquiptableWeapon_SO.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item", menuName = "Scriptable Objects/Item Equiptable Weapon")]
+public class TM_Item_EquiptableWeapon_SO : MonoBehaviour
+{
+    //////////////////////////////// - Prefabs
+
+    [Header("Item Prefabs")]
+    public GameObject UI_Prefab;
+    public GameObject placed_Prefab;
+    public GameObject dropped_Prefab;
+
+    //////////////////////////////// - Consumable Food Stats
+
+    [Header("Item Descriptions")]
+    public string name;
+    public string desc;
+    public int stackSize;
+
+
+    ///////////////////////////////////////////////////////
+
+    public void SpawnItem_Placed()
+    {
+        Debug.Log("Test Code: Spawned");
+
+        //Spawn Object
+        GameObject spawnedItem = Instantiate(dropped_Prefab);
+
+
+        //Position? 
+        //Parents?
+
+
+        //Get Script
+        TM_Item_ConsumableFood_Placed SpawnedItem_Script = spawnedItem.GetComponent<TM_Item_ConsumableFood_Placed>();
+
+        //Construct Values
+        SpawnedItem_Script.name = name;
+        SpawnedItem_Script.desc = desc;
+        SpawnedItem_Script.stackSize = stackSize;
+
+    }
+
+    ///////////////////////////////////////////////////////
+}
