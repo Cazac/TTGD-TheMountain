5c1bf2a6cd52a2bd5bbe2d41f5a104720e638c07
Zac <zachary.blouin@gmail.com>
2020-02-24 18:53:12 -0800

    Systems 0.06
    - Merging Timer UI

--- KPU - The Mountain/Assets/_Progress/TM_Cycle.cs
+++ KPU - The Mountain/Assets/_Progress/TM_Cycle.cs
@@ -1,13 +1,45 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class TM_Cycle : MonoBehaviour
+
+/// <summary>
+/// Class: TM_Cycle
+/// Purpose: To manage the properties of each cycle
+/// </summary>
+public class TM_Cycle
 {
+    /////////////////CYCLE PROPERTIES////////////////////////
+    private static readonly string[] cycleNameList = { "PlayerDMG_Down", "EnemyDMG_Up", "EnemySPD_Up", "PlayerSPD_UP", "PlayerDies" };
+    private static readonly Color[] colorList = { Color.yellow, Color.blue, Color.red, Color.green, Color.white };
+    private static readonly int[] cycleFrequency = { 1, 1, 1, 1, 1 };
+
+    public static readonly int NumOfTotalTypes = 5;
+    public Color cycleColor; //Panel's Current Cycle Color
+    public string cycleName; //Panel's Current Cycle Name
+    ///////////////////////////////////////////////////////
+    public static TM_CycleShuffleBag shuffleBag; //Single instance of the shuffle bag that is shared amongst all TM_Cycle Objects
+
+    public TM_Cycle()
+    {
+        shuffleBag = new TM_CycleShuffleBag(14);
+        for (int x = 0; x < NumOfTotalTypes; x++)
+        {
+            string name = cycleNameList[x];
+            Color c = colorList[x];
+            shuffleBag.AddCycle(new KeyValuePair<string, Color>(name, c), cycleFrequency[x]);
+        }
+    }
+    public void SetRandomCycle()//Sets a random cycle using the Shuffle Bag technique
+    {
+        KeyValuePair<string, Color> nextCycle = shuffleBag.NextCycle();
+        cycleName = nextCycle.Key;
+        cycleColor = nextCycle.Value;
+    }
+    
 
 
 
-
+    //CONTRUCTOR GOES HERE
     public TM_Cycle(TM_CycleSO scriptableItem)
     {
 
