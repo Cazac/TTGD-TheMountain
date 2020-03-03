2b11e6401c144a511af999897ed4a68c258d4c09
Shawn <s.lew@telus.net>
2020-03-02 21:40:15 -0800

    SettingsMenu 0.2
    - Implemented Keybinding Structure
    - Added serializable classes for JSON import
    - Added a resources folder to simplify location of JSON file

--- KPU - The Mountain/Assets/_Scripts/Controllers/Scene - Title/TM_KeyBindingController.cs
+++ KPU - The Mountain/Assets/_Scripts/Controllers/Scene - Title/TM_KeyBindingController.cs
@@ -4,15 +4,100 @@ using UnityEngine;
 
 public class TM_KeyBindingController : MonoBehaviour
 {
+    public static TM_KeyBindingController Instance;
+
+    public KeyCode newKey;
+    public bool waitingForKey = false;//Waits for the key press
+    public GameObject currentButton;
+
+    public static Dictionary<string, KeyCode> keybindings;
+
+    public TextAsset jsonFile;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        jsonFile = Resources.Load("keybindings") as TextAsset; //Loads JSON File
+        keybindings = new Dictionary<string, KeyCode>(); //Initialize keycode dictionary
+        KeyBindingList bindingsInJSON = JsonUtility.FromJson<KeyBindingList>(jsonFile.text);//Convert JSON into serializable strings
+
+        foreach (KeyBinding k in bindingsInJSON.bindings)
+        {
+            Debug.Log("Found binding: " + k.key + " for  " + k.action);
+
+            keybindings.Add(k.action, (KeyCode)System.Enum.Parse(typeof(KeyCode), k.key));
+        }
+        Debug.Log("Successfully parsed initial JSON bindings");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        
+        Event e = Event.current; //Captures anything that is happening in the game
+        if (e.shift && waitingForKey)
+        {
+            Debug.Log(e.modifiers);
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                newKey = KeyCode.LeftShift;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                newKey = KeyCode.RightShift;
+            }
+            waitingForKey = false; //Stop waiting for key
+        }
+        else if (e.isKey && waitingForKey)
+        {
+            newKey = e.keyCode; //Set that key as keycode
+            waitingForKey = false; //Stop waiting for key
+        }
+    }
+
+    public void ChangeKeyStart(GameObject button)
+    {
+        if (!waitingForKey)//Allows only one binding to be changed at a time
+        {
+            currentButton = button;
+            Debug.Log(currentButton.name + " is pressed");
+            waitingForKey = true;
+            Debug.Log("Current Binding: " + keybindings[button.name]);
+            StartCoroutine(EditKeyFlag());
+        }
+    }
+
+    public void ChangeKeyEnd()
+    {
+        Debug.Log(newKey);
+        StopCoroutine(EditKeyFlag());//Save potential resources
+        Debug.Log("end coroutine");        
+    }
+
+    IEnumerator EditKeyFlag() //Coroutine to loop while we wait for a key to be pressed
+    {
+        Debug.Log("start coroutine");
+        while (waitingForKey)
+        {
+            yield return null;
+        }
+        ChangeKeyEnd();
+    }
+
+    [System.Serializable]
+    public class KeyBinding
+    {
+        //these variables are case sensitive and must match the strings in the JSON.
+        public string action;
+        public string key;
+    }
+
+    [System.Serializable]
+    public class KeyBindingList
+    {
+        //bindings is case sensitive and must match the string "bindings" in the JSON.
+        public KeyBinding[] bindings;
     }
 }
