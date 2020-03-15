5b55876447033bb7724ab4615dd24758e81af8f7
Shawn <s.lew@telus.net>
2020-03-14 23:26:54 -0700

    SettingsMenu 0.3
    - Fixed detection of Shift Key during key change
    - Added auto-saving key changes
    - Updated load functionality:
    Keybinds will be loaded from persistentDataPath, if it exists
    Otherwise, Keybinds will be loaded from "default" JSON file in Resources folder
    - Cleaned up code

--- KPU - The Mountain/Assets/_Scripts/Controllers/Scene - Title/TM_KeyBindingController.cs
+++ KPU - The Mountain/Assets/_Scripts/Controllers/Scene - Title/TM_KeyBindingController.cs
@@ -4,53 +4,74 @@ using UnityEngine;
 
 public class TM_KeyBindingController : MonoBehaviour
 {
+    public static string KB_PATH; //Uses a persistent data path that will never change because it is based on the system, not the application
     public static TM_KeyBindingController Instance;
 
-    public KeyCode newKey;
-    public bool waitingForKey = false;//Waits for the key press
-    public GameObject currentButton;
+    public GameObject BindingInputs;//Reference for the initial button text names
+
+    GameObject currentButton;//Used OnClick() to hold a reference to button that is currently being changed
+    KeyCode newKey;//New key to be set
+    bool waitingForKey = false;//Flag to wait for the key press
 
     public static Dictionary<string, KeyCode> keybindings;
 
-    public TextAsset jsonFile;
+    string jsonFile;//JSON File used to initialize dictionary
+    KeyBindingList bindingsInJSON;
 
     private void Awake()
     {
         Instance = this;
+        KB_PATH = Application.persistentDataPath + "/keybindings.json";
     }
     // Start is called before the first frame update
     void Start()
     {
-        jsonFile = Resources.Load("keybindings") as TextAsset; //Loads JSON File
+        if (System.IO.File.Exists(KB_PATH))//Tries to find saved JSON file in cache
+        {
+            jsonFile = System.IO.File.ReadAllText(KB_PATH);
+            Debug.Log("Loaded bindings from saved file");
+        }
+        else //If not, loads default JSON file
+        {
+            TextAsset jsonAsset = Resources.Load("keybindings") as TextAsset;
+            jsonFile = jsonAsset.text;
+            Debug.Log("Loaded default bindings");
+        }
         keybindings = new Dictionary<string, KeyCode>(); //Initialize keycode dictionary
-        KeyBindingList bindingsInJSON = JsonUtility.FromJson<KeyBindingList>(jsonFile.text);//Convert JSON into serializable strings
+        bindingsInJSON = JsonUtility.FromJson<KeyBindingList>(jsonFile);//Convert JSON into serializable strings
 
         foreach (KeyBinding k in bindingsInJSON.bindings)
         {
             Debug.Log("Found binding: " + k.key + " for  " + k.action);
 
-            keybindings.Add(k.action, (KeyCode)System.Enum.Parse(typeof(KeyCode), k.key));
+            keybindings.Add(k.action, (KeyCode)System.Enum.Parse(typeof(KeyCode), k.key));//Adds binding to dictionary
         }
         Debug.Log("Successfully parsed initial JSON bindings");
+
+        //Setting initial loaded bindings as button texts
+        for(int i = 0; i < BindingInputs.transform.childCount; i++)
+        {
+            GameObject button = BindingInputs.transform.GetChild(i).gameObject;
+            button.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().SetText(keybindings[button.name].ToString());
+        }
     }
 
     void OnGUI()
     {
-        Event e = Event.current; //Captures anything that is happening in the game
-        if (e.shift && waitingForKey)
+        Event e = Event.current; //Captures anything that is happening in game
+        if (e.shift && waitingForKey)//If Shift is pressed as key
         {
-            Debug.Log(e.modifiers);
-            if (Input.GetKeyDown(KeyCode.LeftShift))
+            if (Input.GetKey(KeyCode.LeftShift))//Left Shift
             {
                 newKey = KeyCode.LeftShift;
             }
-            else if (Input.GetKeyDown(KeyCode.RightShift))
+            else if (Input.GetKey(KeyCode.RightShift))//Right Shift
             {
                 newKey = KeyCode.RightShift;
             }
-            waitingForKey = false; //Stop waiting for key
+            waitingForKey = false;
         }
-        else if (e.isKey && waitingForKey)
+        else if (e.isKey && waitingForKey)//Everything else
         {
             newKey = e.keyCode; //Set that key as keycode
             waitingForKey = false; //Stop waiting for key
@@ -62,23 +83,40 @@ public class TM_KeyBindingController : MonoBehaviour
         if (!waitingForKey)//Allows only one binding to be changed at a time
         {
             currentButton = button;
-            Debug.Log(currentButton.name + " is pressed");
             waitingForKey = true;
-            Debug.Log("Current Binding: " + keybindings[button.name]);
             StartCoroutine(EditKeyFlag());
         }
     }
 
     public void ChangeKeyEnd()
     {
-        Debug.Log(newKey);
+        if (!keybindings.ContainsValue(newKey))//Checks if binding already exists for a different key (or the current key)
+        {
+            keybindings[currentButton.name] = newKey;//Sets new key as part of binding dictionary
+            SaveToJSON();
+            currentButton.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().SetText(keybindings[currentButton.name].ToString());
+        }
         StopCoroutine(EditKeyFlag());//Save potential resources
-        Debug.Log("end coroutine");        
+    }
+    //Converts current Dictionary structure into JSON string and saves into file
+    public void SaveToJSON()
+    {
+        List<string> bindingActions = new List<string>(keybindings.Keys);//Grab only keys
+        List<KeyBinding> bindingKeys = new List<KeyBinding>();//Create list of KeyBinding class/struct
+        foreach (string a in bindingActions)
+        {
+            bindingKeys.Add(new KeyBinding() {action = a, key = keybindings[a].ToString()});//Add each keybinding from dictionary into a List of struct KeyBinding
+        }
+        KeyBindingList saveBindingList = new KeyBindingList(){bindings = bindingKeys.ToArray()};//Convert into KeyBindingList
+
+        string json = JsonUtility.ToJson(saveBindingList);//Convert to JSON
+        System.IO.File.WriteAllText(KB_PATH, json);//Writes to file
+        Debug.Log("Saved to File");
     }
 
-    IEnumerator EditKeyFlag() //Coroutine to loop while we wait for a key to be pressed
+    //Coroutine to loop while we wait for a key to be pressed TODO: Maybe add a way to prevent other things from being able to be pressed?
+    IEnumerator EditKeyFlag()
     {
-        Debug.Log("start coroutine");
         while (waitingForKey)
         {
             yield return null;
@@ -86,6 +124,7 @@ public class TM_KeyBindingController : MonoBehaviour
         ChangeKeyEnd();
     }
 
+    //Serialized structure used in parsing JSON
     [System.Serializable]
     public class KeyBinding
     {
@@ -93,7 +132,7 @@ public class TM_KeyBindingController : MonoBehaviour
         public string action;
         public string key;
     }
-
+    //Serialized Array structure used in parsing JSON
     [System.Serializable]
     public class KeyBindingList
     {
